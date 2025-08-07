d98e80a03bc7c77e4362b37c727379bbf045db2d
Gian Cristian Baguna <usernamenigian@gmail.com>
2025-08-08 03:31:05 +0800

    [Gian][Dependency] added config dependecies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  safelist: [
+    "bg-blue-500",
+    "bg-green-500",
+    "bg-yellow-500",
+    "bg-red-500",
+    "text-white",
+    "border-blue-500",
+    "border-green-500",
+    "border-yellow-500",
+    "border-red-500",
+  ],
   theme: {
     extend: {
       skew: {
